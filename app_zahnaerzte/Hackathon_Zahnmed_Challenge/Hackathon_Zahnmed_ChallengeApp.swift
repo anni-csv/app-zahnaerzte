762c2f9cc4303e24160f12d7118607cd6f73ed9c
anni-csv <dbbp7j9djd@privaterelay.appleid.com>
2026-05-22 14:11:05 +0200

    Add files via upload

---
+++ app_zahnaerzte/Hackathon_Zahnmed_Challenge/Hackathon_Zahnmed_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hackathon_Zahnmed_ChallengeApp.swift
+//  Hackathon_Zahnmed_Challenge
+//
+//  Created by Felix Pradel on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct Hackathon_Zahnmed_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
